21bd0aff9b4eeeb2f3fb79bbaae0f605d80a483e
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-10-03 16:08:11 +0300

    up files;

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import OnlyScrollbar from 'only-scrollbar';
+
+// const scroll = new OnlyScrollbar('.scroll-container');
+// scroll.destroy();
